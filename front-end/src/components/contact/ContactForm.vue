e693f2dfc340c17033325b8fcd20e36d9aa028c1
BabouZ17 <dimitri.estrade@gmail.com>
2019-10-21 22:48:03 -0400

    dev, adding content on contact viez

--- front-end/src/components/contact/ContactForm.vue
+++ front-end/src/components/contact/ContactForm.vue
@@ -1,79 +1,71 @@
 <template>
   <v-form v-model="valid">
     <br />
-    <v-flex 
-      md6
-      offset-md3
-    >
-    <v-card>
-      <v-card-title>
-        Contact
-      </v-card-title>
-      <v-card-text>
-        <v-container 
-          grid-list-xl
-          justify-center=True
-        >
-          <v-layout wrap>
-            <v-flex
-              xs12
-              md6
-              offset-md3
-            >
-              <v-text-field
-                v-model="firstname"
-                :rules="nameRules"
-                :counter="10"
-                label="First name"
-                required
-              ></v-text-field>
-            </v-flex>
+    <v-flex md6 offset-md3>
+      <v-card>
+        <v-card-title>Contact</v-card-title>
+        <v-card-text>
+          <v-container grid-list-xl justify-center="True">
+            <v-layout wrap>
+              <v-flex xs12 md6 offset-md3>
+                <v-text-field
+                  v-model="firstName"
+                  :rules="nameRules"
+                  :counter="25"
+                  label="First name"
+                  required
+                ></v-text-field>
+              </v-flex>
 
-            <v-flex
-              xs12
-              md6
-              offset-md3
-            >
-              <v-text-field
-                v-model="lastname"
-                :rules="nameRules"
-                :counter="10"
-                label="Last name"
-                required
-              ></v-text-field>
-            </v-flex>
+              <v-flex xs12 md6 offset-md3>
+                <v-text-field
+                  v-model="lastName"
+                  :rules="nameRules"
+                  :counter="25"
+                  label="Last name"
+                  required
+                ></v-text-field>
+              </v-flex>
 
-            <v-flex
-              xs12
-              md6
-              offset-md3
-            >
-              <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
-    </v-card>
+              <v-flex xs12 md6 offset-md3>
+                <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+              </v-flex>
+
+              <v-flex xs12 md6 offset-md3>
+                <v-textarea v-model="text" label="Text" auto-grow required></v-textarea>
+              </v-flex>
+            </v-layout>
+            <v-layout justify-center="True">
+              <v-btn :disabled="!valid" color="success" class="mr-4" @click="submit">Validate</v-btn>
+              <v-btn color="red" class="mr-4" @click="reset">Reset</v-btn>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-form>
 </template>
 
 <script>
-    export default {
-      props: ["emailRules", "nameRules", "valid"],
-      data: function(){
-        return {
-          email: "",
-          firstname: "",
-          lastname: ""
-        }
-      }
-    }
+export default {
+  data: function() {
+    return {
+      email: "",
+      firstName: "",
+      lastName: "",
+      text: "",
+      emailRules: [
+        v => !!v || "Email is required",
+        v => (v && v.length <= 50) || "Email must be..."
+      ],
+      nameRules: [
+        v => !!v || "Name is required",
+        v => (v && v.length <= 25) || "Name must be..."
+      ],
+      valid: false
+    };
+  }
+};
 </script>
 
 
